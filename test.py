decfab81eb8b7842a7e1f3474ee95e4eb4748395
jeff <jeff@jeffgraham.com>
2017-06-25 18:18:38 -0400

    loop 500

--- test.py
+++ test.py
@@ -15,3 +15,22 @@ print "LED 17 off"
 GPIO.output(17,GPIO.LOW)
 print "LED 27 off"
 GPIO.output(27,GPIO.LOW)
+
+time.sleep(1)
+
+s = 0.15
+ff = 1
+for _ in range(500):
+    if ff == 0:
+        ff = 1
+        print "LED 17 on"
+        GPIO.output(17,GPIO.HIGH)
+        print "LED 27 on"
+        GPIO.output(27,GPIO.HIGH)
+    else:
+        ff = 0
+        print "LED 17 off"
+        GPIO.output(17,GPIO.LOW)
+        print "LED 27 off"
+        GPIO.output(27,GPIO.LOW)
+    time.sleep(s)
