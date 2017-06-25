fbef81cc3a5267d21570f4b72830f1fcfc3efcf7
jeff <jeff@jeffgraham.com>
2017-06-25 18:11:39 -0400

    2 leds now

--- test.py
+++ test.py
@@ -3,8 +3,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
-print "LED on"
+
+print "LED 17 on"
 GPIO.output(17,GPIO.HIGH)
+print "LED 27 on"
+GPIO.output(27,GPIO.HIGH)
+
 time.sleep(1)
-print "LED off"
+print "LED 17 off"
 GPIO.output(17,GPIO.LOW)
+print "LED 27 off"
+GPIO.output(27,GPIO.LOW)
