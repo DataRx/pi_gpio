d175c36c666169edb9061625bbeae86add884a38
jeff <jeff@jeffgraham.com>
2017-06-25 18:22:08 -0400

    faster

--- test.py
+++ test.py
@@ -51,3 +51,37 @@ for _ in range(50):
         print "LED 27 on"
         GPIO.output(27,GPIO.HIGH)
     time.sleep(s)
+
+s = 0.10
+print 'faster 0.10'
+for _ in range(50):
+    if ff == 0:
+        ff = 1
+        print "LED 17 on"
+        GPIO.output(17,GPIO.HIGH)
+        print "LED 27 off"
+        GPIO.output(27,GPIO.LOW)
+    else:
+        ff = 0
+        print "LED 17 off"
+        GPIO.output(17,GPIO.LOW)
+        print "LED 27 on"
+        GPIO.output(27,GPIO.HIGH)
+    time.sleep(s)
+
+s = 0.05
+print 'faster 0.05'
+for _ in range(50):
+    if ff == 0:
+        ff = 1
+        print "LED 17 on"
+        GPIO.output(17,GPIO.HIGH)
+        print "LED 27 off"
+        GPIO.output(27,GPIO.LOW)
+    else:
+        ff = 0
+        print "LED 17 off"
+        GPIO.output(17,GPIO.LOW)
+        print "LED 27 on"
+        GPIO.output(27,GPIO.HIGH)
+    time.sleep(s)
